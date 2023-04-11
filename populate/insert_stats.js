80348730447d6b64c33e2c737a69cef62290d15e
BryleBanatin <75974322+SamohtB@users.noreply.github.com>
2023-04-11 17:05:09 +0800

    Insert Stats and python script
    use:
    node populate/insert_stats

---
+++ populate/insert_stats.js
@@ -0,0 +1,43 @@
+const {PythonShell} = require("python-shell");
+const mysql = require('mysql2');
+const { options } = require("../src/routes/routes");
+require("dotenv").config();
+
+let shell = new PythonShell('populate/unite_pokemon_stats.py');
+
+const pool = mysql.createConnection({
+  host: process.env.SQL_HOST,
+  user: 'root',
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DB
+});
+
+options.JSON;
+
+shell.on('message', function(message){
+
+  pool.connect(function(err) {  
+
+    if (err) throw err;   
+    sql = "INSERT INTO Stats(ownerid, level, HP, ATK, DEF, SpA, SpD, criticalrate, cooldownredux, lifesteal, attackspeed) VALUES ROW (?)";
+    message = JSON.parse(message);
+    values = [message["ownerid"], message["level"], message["HP"], message["ATK"], message["DEF"], message["SpA"], message["SpD"], 
+              message["criticalrate"], message["cooldownredux"], message["lifesteal"], message["attackspeed"]];
+    pool.query(sql, [values], function (err, result) {
+      if(err)
+      {
+          console.log("An error occured");
+          console.error(err);
+          return;
+      }
+      console.log(result);
+    });  
+    });  
+});
+
+shell.end(function (err,code,signal) {
+  if (err) throw err;
+  console.log('The exit code was: ' + code);
+  console.log('The exit signal was: ' + signal);
+  console.log('finished');
+});
---
+++ populate/unite_pokemon_stats.py
@@ -0,0 +1,25 @@
+from py_unite_db import UniteDb
+import json
+
+unite_db = UniteDb()
+
+index = 1
+
+for pokemon in unite_db.pokemon:
+    
+    for level in range(15):
+        element = {
+            "ownerid" : index, 
+            "level" : level + 1, 
+            "HP" : pokemon.stats_at(level + 1).hp,
+            "ATK" : pokemon.stats_at(level + 1).attack,
+            "DEF" : pokemon.stats_at(level + 1).defense,
+            "SpA" : pokemon.stats_at(level + 1).sp_attack,
+            "SpD" : pokemon.stats_at(level + 1).sp_defense,
+            "criticalrate" : pokemon.stats_at(level + 1).crit,
+            "cooldownredux" : pokemon.stats_at(level + 1).cooldown_reduction, 
+            "lifesteal" : pokemon.stats_at(level + 1).lifesteal
+        }
+        print(json.dumps(element))
+    
+    index += 1
